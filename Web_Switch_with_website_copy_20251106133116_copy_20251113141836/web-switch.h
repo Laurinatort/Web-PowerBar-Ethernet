3875916237fcfe6f1d3ad5f873612d8e284c26cd
Laurin Gehrig <laurinatortvretro@gmail.com>
2026-01-14 13:23:08 +0100

    web-switch.h
    translated the discription

--- Web_Switch_with_website_copy_20251106133116_copy_20251113141836/web-switch.h
+++ Web_Switch_with_website_copy_20251106133116_copy_20251113141836/web-switch.h
@@ -1,27 +1,27 @@
-#include <SPI.h>                 // SPI-Schnittstelle für Ethernet
-#include <Ethernet.h>            // Ethernet Stack für W5x00-Chips
-#include <FlashAsEEPROM_SAMD.h>  // EEPROM-Emulation für SAMD21
+#include <SPI.h>                 // SPI interface for Ethernet
+#include <Ethernet.h>            // Ethernet Stack for W5x00 chips
+#include <FlashAsEEPROM_SAMD.h>  // EEPROM emulation for SAMD21
 
-#define ETH_CS 5 // Chip-Select Pin des Ethernet-Controllers
+#define ETH_CS 5 // Ethernet controller Chip-Select pin
 #define EEPROM_EMULATION_SIZE 4096
 
 class Relay{
   public:
-    bool state[6]; // Status der sechs Relais (true = ein / false = aus)
-    static inline constexpr  int pin[6]  = { 0, 1, 2, 3, 6, 7 };  // Zuordnung der digitalen Pins zu den Relais
-    String tglex; // Hilfsstring zum Erkennen von "toggle-relayX"
+    bool state[6]; // Status of the six relays (true = on / false = off)
+    static inline constexpr  int pin[6]  = { 0, 1, 2, 3, 6, 7 };  // Mapping the digital pins to the relays
+    String tglex; // Helper string for detecting "toggle-relayX"
     void power();
     void toggle(EthernetClient &client, int i);
 } relay;
 
 class Button{
   public:
-    static inline constexpr  int pin[6] = { 15, 16, 17, 18, 19, 20 }; // Zuordnung der digitalen Pins zu den Tastern
-    int pressed[6];  // Entprell-Status der Taster (0 = nicht gedrückt, 1 = gedrückt)
+    static inline constexpr  int pin[6] = { 15, 16, 17, 18, 19, 20 }; // Mapping the digital pins to the buttons
+    int pressed[6];  // Debounce status of the buttons (0 = not pressed, 1 = pressed)
     void detect(Relay& relay);
 } button;
 
-// Standard-Netzwerkparameter
+// Standard network parameters
 class IPS {
   public:
     byte ip[4];
@@ -35,8 +35,8 @@ class IPS {
     }
 };
 IPS standart((byte[4]){192, 168, 1, 177}, (byte[4]){192, 168, 1, 1}, (byte[4]){255, 255, 255, 0});
-IPS temp; // Temporäre Speicher für neue Netzwerkkonfigurationen
-byte mac[] = { 0x00, 0x04, 0xA3, 0x00, 0x00, 0x06 };  // MAC-Adresse des Ethernet-Shields
+IPS temp; // Temporary storage for new network configurations
+byte mac[] = { 0x00, 0x04, 0xA3, 0x00, 0x00, 0x06 };  // MAC address of the Ethernet shield
 
 class Cfg {
   public:
@@ -45,15 +45,16 @@ class Cfg {
     int reset();
 } config;
 
-// Interne Hilfsvariablen
+// Internal helper variables
 int chnge = 0;
 char c;
-String cfgexp[2] = { "ip", "subnet" };  // Strings für Konfigurationserkennung im HTTP-POST
-byte first = 1;                                       // Flag für „erste Initialisierung“
-int resetq = 0;                                       // Variablen für Reset-Erkennung
-String readString;                                    // Puffer für eingehende HTTP-Anfragen
+String cfgexp[2] = { "ip", "subnet" };  // Strings for configuration detection in HTTP-POST
+byte first = 1;                         // Flag for "first initialization"
+int resetq = 0;                         // Variables for reset detection
+String readString;                      // Buffer for incoming HTTP requests
 
 /************************************************************
-** Ethernet-Server Instanz auf Port 80                      
+** Ethernet Server Instance on Port 80                      
 ************************************************************/
+
 EthernetServer server(80);
